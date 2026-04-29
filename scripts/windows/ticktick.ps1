7d68e572fc5c5cf89bf5006fa567226589473768
Kamil <kamil.prz@gmail.com>
2026-04-29 23:47:57 +0100

    remove garbage

--- scripts/windows/ticktick.ps1
+++
@@ -1,5 +0,0 @@
-
-# winapp get ticktick
-# create firewall rule to block ticktick traffic from leaving the computer
-# rename the original ticktick executable
-# create a shortcut which runs the app and checks whether the firewall rule is enabled
